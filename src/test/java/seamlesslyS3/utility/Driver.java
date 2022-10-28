c7b6247e175625f56ac05176d118caa1ea17da06
YY <ysf.94.yldrm@gmail.com>
2022-10-28 11:54:01 +0300

    @SEAMLES-2418 remote chrome edit

--- src/test/java/seamlesslyS3/utility/Driver.java
+++ src/test/java/seamlesslyS3/utility/Driver.java
@@ -48,7 +48,7 @@ public class Driver {
                 case"remote-chrome":
                     try {
                         //assign your grid server address
-                        String gridAddress = "";
+                        String gridAddress = "3.208.8.137";
                         URL url = new URL("http://"+gridAddress + ":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("chrome");
